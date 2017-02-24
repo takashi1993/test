1b7c342b4f78e064df83c079c9ff5b1ad1847a89
yutaka <y@y>
2017-02-24 10:48:21 +0900

    iostreamに変更

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,18 @@
-#include <stdio.h>
+//#include <stdio.h>
+#include <iostream>
 
 void test(){
     //テスト関数
 
-    printf("test functions");
+    //printf("test functions");
+
+    std::cout << "test functions" << std::endl ;
 
     int b = 10;
     b++;
     b+=100;
+}
+
+int main(){
+    test();
 }
